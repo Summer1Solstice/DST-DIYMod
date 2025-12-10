ba6f93fdd41f1989bc07051e47721de4e6a4e69c
laptop <46267607+Summer1Solstice@users.noreply.github.com>
2025-12-10 15:19:42 +0800

    Changes to be committed: 	modified:   DIY_infinite/modmain.lua

--- DIY_infinite/modmain.lua
+++ DIY_infinite/modmain.lua
@@ -1,14 +1,26 @@
+-- 无限耐久
 local function infiniteMod(inst)
     local self1 = inst.components.finiteuses
     if self1 then
         function self1:Use(num) return true end
 
-        if self1.total < 5 then
-            self1.total = 10
+        print("infiniteMod: ", inst.prefab)
+        local uses = GLOBAL.TUNING[string.upper(inst.prefab .. "_USES")]
+        if uses ~= nil and self1.total < uses then
+            uses = math.ceil(uses / 2)
+            print("infiniteMod: ", inst.prefab, "=", uses)
+            self1.total = uses
+            self1:SetUses(self1.total)
         end
-        self1:SetUses(self1.total)
+        -- if self1.total < 5 then
+        --     self1.total = 10
+        -- end
+        -- self1:SetUses(self1.total)
     end
 end
-
--- 应用到多个物品
-AddPrefabPostInit("greenstaff", infiniteMod)
+-- 创建一个数组
+local infiniteItems = { "greenstaff" }
+-- 遍历数组
+for i = 1, #infiniteItems do
+    AddPrefabPostInit(infiniteItems[i], infiniteMod)
+end
